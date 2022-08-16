a339dc31b29dfad9da053eeb093760bd1c4d3bef
ionite34 <dev@ionite.io>
2022-08-16 00:31:29 -0400

    Improved 2021/11 solution

--- src/aoc/2021/11/solution.py
+++ src/aoc/2021/11/solution.py
@@ -1,8 +1,12 @@
 # advent of code 2021
 # https://adventofcode.com/2021
 # day 11
+import logging
 
 import numpy as np
+import numpy.ma as ma
+from funcy import print_durations
+from numba import njit
 from scipy.ndimage import convolve
 
 NEIGHBORS = np.array([
@@ -11,48 +15,42 @@ NEIGHBORS = np.array([
     [1, 1, 1]
 ])
 
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
-def parse_input(lines) -> np.matrix:
+
+def parse_input(lines) -> ma.MaskedArray:
     mat = np.matrix(';'.join(' '.join(s) for s in lines), dtype=np.int16)
-    return mat
+    return ma.masked_array(mat)
 
 
-def run(data: np.matrix, cycles: int) -> tuple[int, list]:
-    print(f"Before Steps:\n{data}")
+def run(data: ma.MaskedArray, cycles: int) -> tuple[int, list]:
+    log.info(f"Before Steps:\n{data}")
     flashes = 0
     all_flashes = []  # Steps where all cells were zero (flashed)
     for i in range(cycles):
-        # First add 1 to all cells
         data += 1
-        while np.any(data > 9):  # Loop until no more changes
-            # For all cells that are 10, these are 'flashing' cells
-            # Do convolution to raise all adjacent cells by 1
-            flashing = (data > 9)
-            data += convolve(
-                flashing.astype(np.int16),
-                NEIGHBORS,
-                mode='constant',
-            )
-            # print(f"Flashing {i}:\n{data}")
-            # Set all flashed cells to -100
-            data[flashing] = -100
+        while np.any(flashing := (data > 9 & ~data.mask)):  # Loop until no changes
+            data += convolve(flashing.astype(np.int16), NEIGHBORS, mode='constant')
+            data.mask |= flashing
 
-        # Set all flashed cells (<0) to 0
-        flashes += np.sum(data < 0)
-        data[data < 0] = 0
+        if data.mask.all():  # All flashed states
+            all_flashes.append(i + 1)
 
-        # Detect all flashes
-        if np.all(data == 0):
-            all_flashes.append(i+1)
+        flashes += np.count_nonzero(data.mask)
+        data.data[data.mask] = 0  # Set masked to 0
+        data.mask = False  # Reset mask
 
-        print(f"=-=-=\nAfter Step {i+1}:\n{data}")
-    print(f"All flash at: {all_flashes}")
+        log.info(f"=-=-=\nAfter Step {i + 1}:\n{data}")
+    log.info(f"All flash at: {all_flashes}")
     return flashes, all_flashes
 
 
-def part1(data: np.matrix) -> int:
+@print_durations
+def part1(data: ma.MaskedArray) -> int:
     return run(data.copy(), 100)[0]
 
 
-def part2(data: np.matrix) -> int:
+@print_durations
+def part2(data: ma.MaskedArray) -> int:
     return run(data.copy(), 370)[1][0]
