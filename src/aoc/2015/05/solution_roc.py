d09dd1416cf754e4a65d9d62ca0d2a0d176afa7f
ionite34 <dev@ionite.io>
2022-08-17 00:40:56 -0400

    Updated solution for 2015/05

--- src/aoc/2015/05/solution_roc.py
+++ src/aoc/2015/05/solution_roc.py
@@ -2,15 +2,15 @@ import re
 
 
 def part1(data: list[str]) -> int:
-    return sum(map(lambda x: bool(
+    return sum(bool(
         not re.search(r'(ab|cd|pq|xy)', x) and
         re.search(r'([a-z])(\1)', x) and
         len(re.findall(r'[aeiou]', x)) >= 3
-    ), data))
+    ) for x in data)
 
 
 def part2(data: list[str]) -> int:
-    return sum(map(lambda x: bool(
+    return sum(bool(
         re.search(r'([a-z])[a-z](\1)', x) and
         re.search(r'([a-z])([a-z]).*(\1)(\2)', x)
-    ), data))
+    ) for x in data)
